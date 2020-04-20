269c23834345e357dd6e25d33f04f4df1362aa8e
Antonio Guimaraes <antonioguimaraes@MacBook-Pro-de-Antonio.local>
2020-04-20 14:55:43 -0300

    add composer

---
+++ boot.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+(new Vendala\SDKLog\HelloWorld)->handler();
--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ErikFig;
+namespace Vendala\SDKLog;
 
 class HelloWorld
 {
